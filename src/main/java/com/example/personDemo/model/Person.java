bf0c69ae19e11e4a1e1d5745c27db483dd17d831
yogesh <yogeshdewade860@gmail.com>
2023-07-23 00:01:36 +0530

    Second Commit

--- src/main/java/com/example/personDemo/model/Person.java
+++ src/main/java/com/example/personDemo/model/Person.java
@@ -9,7 +9,7 @@ import lombok.Data;
 
 public class Person {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private  int id;
     private String personName;
     private String cityName;
